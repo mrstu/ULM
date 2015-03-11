a6d07dc00959f4a50532124f56d6bebec67bc4c0
Matt Stumbaugh <mrstu@u.washington.edu>
2015-03-11 15:06:20 -0700

    Require that snowbands and elevation average elevation are the same

--- driver/READ_SNOWBANDS.f90
+++ driver/READ_SNOWBANDS.f90
@@ -18,7 +18,7 @@ SUBROUTINE READ_SNOWBANDS()
   ! Define local variables
   INTEGER :: I,J
   INTEGER :: tmp_cellid
-  REAL    :: total_area, total_prec, avg_elev
+  REAL    :: total_area, total_prec, avg_elev, band_elev_wgt,band_elev_sum,bandlapsed_band,bandlapsed_cell
 
 
   ! Get band parameters
@@ -65,7 +65,23 @@ SUBROUTINE READ_SNOWBANDS()
     avg_elev = avg_elev / total_area
     ! arbitrary small difference threshold between cell/band sourced avg elevation, 5 meters.
     IF (ABS(avg_elev - ELEV_2d(I)) > 0.1) THEN
-        write(*,'Cell elevation (f5.3) not equal to weighted band average elevation (f5.3)')ELEV_2d(I),avg_elev
+        write(*,'("Error: cell elevation not equal to band averaged elevation")')
+        write(*,'("Cell elevation:",f10.2)')ELEV_2d(I)
+        write(*,'("Band average elevation:",f10.2)')avg_elev
+        band_elev_sum = 0
+        write(*,'("Band <2 bandnum>: <3 band_area>, <4 band_elev_wgt>, <5 band_elev>, <6-7 lapse band rel {bands,cell} average>, <8-9 height diff band-{band_avg, cell_avg}>)")')
+        DO J = 1, nbands
+          IF (band_area(I,J) > 0) THEN
+            band_elev_wgt = band_area(I,J) / total_area
+            band_elev_sum = band_elev_sum + band_elev(I,J) * band_elev_wgt
+            bandlapsed_band = (avg_elev-band_elev(I,J)) / 1000. * LAPSE_RATE
+            bandlapsed_cell = (ELEV_2d(I)-band_elev(I,J)) / 1000. * LAPSE_RATE
+            write(*,'("Band",i2,": ",f5.2, f5.2, f10.1, f6.2, f6.2, f8.2, f8.2)')J,band_area(I,J),band_elev_wgt,band_elev(I,J),bandlapsed_band,bandlapsed_cell,(band_elev(I,J)-avg_elev),(band_elev(I,J)-ELEV_2d(I))
+          END IF
+        END DO
+        ! write(*,'("Band ",i2,": ",f8.2,f8.2,f8.2,f8.2,f8.2)')J,band_elev(I,J),band_area(I,J),band_elev_wgt,band_elev_sum, band_elev_sum-ELEV_2d(I), band_elev_sum-ELEV_2d(I)
+        ! write(*,'("Cell elevation (",f8.2,") not equal to weighted band average elevation (",f8.2,")")')ELEV_2d(I),avg_elev
+        write(*,'("Now exiting with signal 12")')
         STOP 12
     !    ELEV_2d(I)=avg_elev
     END IF
